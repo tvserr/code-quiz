bf9ef8f01cd6658f7f8767c9d1eb96ba9a6ee46e
Joel Serrano <theavserrano@gmail.com>
2023-09-19 02:05:57 -0600

    displayed highscores and timer

--- assets/script.js
+++ assets/script.js
@@ -3,16 +3,19 @@ var questionContainer = document.querySelector(".question-container");
 var choicesContainer = document.querySelector(".choices-container");
 var scoresContainer = document.querySelector(".scores-container");
 
-var startBtn = document.getElementById("#start-quiz");
-var optionBtn1 = document.getElementById("#option1");
-var optionBtn2 = document.getElementById("#option2");
-var optionBtn3 = document.getElementById("#option3");
-var optionBtn4 = document.getElementById("#option4");
-var userScore = document.getElementById("#user-score");
-var questionTitle = document.getElementById("#question-title");
+var startBtn = document.getElementById("start-quiz");
+var timerDisplay = document.getElementById("timer-display");
+var optionBtn1 = document.getElementById("option1");
+var optionBtn2 = document.getElementById("option2");
+var optionBtn3 = document.getElementById("option3");
+var optionBtn4 = document.getElementById("option4");
+var userScore = document.getElementById("user-score");
+var questionTitle = document.getElementById("question-title");
 
 var currentQuestion = 0;
 var score = 0;
+var timerCount = 75;
+var timerInterval;
 
 var questions = [
    {
@@ -26,32 +29,40 @@ var questions = [
    answer: "b. false",
    },
    {
-      title:
-      "In Javascript, this executes a block of code over and over again. What is this called?",
-   choices: [
-      "a. variable loop",
-      "b. function loop",
-      "c. string loop",
-      "d. for loop",
-   ],
+   title: "In Javascript, this executes a block of code over and over again. What is this called?",
+   choices: ["a. variable loop", "b. function loop", "c. string loop", "d. for loop"],
    answer: "d. for loop",
    },
    {
-      title: "In Javascript, what is this container that stores data?",
-      choices: ["a. variable", "b. function", "c. string", "d. event"],
+   title: "In Javascript, what is this container that stores data?",
+   choices: ["a. variable", "b. function", "c. string", "d. event"],
    answer: "a. variable",
    },
 ];
 
-optionBtn1.addEventListener("click");
-optionBtn2.addEventListener("click");
-optionBtn3.addEventListener("click");
-optionBtn4.addEventListener("click");
+// optionBtn1.addEventListener("click");
+// optionBtn2.addEventListener("click");
+// optionBtn3.addEventListener("click");
+// optionBtn4.addEventListener("click");
 
 function startQuiz() {
    currentQuestion = 0;
    score = 0;
+   timerCount = 75;
+   startTimer();
    displayQuestion(currentQuestion);
+
+}
+
+function startTimer() {
+   timerInterval = setInterval(function () {
+      timerCount--;
+      timerDisplay.textContent = "Time: " + timerCount;
+
+      if (timerCount <= 0) {
+         endQuiz();
+      }
+   }, 1000)
 }
 
 function displayQuestion(questionIndex) {
@@ -77,16 +88,28 @@ function checkAnswer(event) {
    var currentQuestionObj = questions[currentQuestion];
 
    if (userChoice != currentQuestionObj.answer) {
-   score -= 15;
-   userScore.textContent = "Score: " + score;
+   timerCount -= 15;
+   userScore.textContent = "Your final score is " + score + ".";
    }
 
    currentQuestion++;
    displayQuestion(currentQuestion);
 }
 
+function showScore() {
+   var correctAnswers = 0;
+
+   questions.forEach(function (currentQuestion, currentQuestionIndex) {
+      if (currentQuestion.choices.indexOf(currentQuestion.answer) !== -1) {
+         correctAnswers++;
+      }
+   })
+
+   scoresContainer.innerHTML = "Your final score is " + correctAnswers + ".";
+}
+
 function endQuiz() {}
 
-startBtn.addEventListener("click", startQuiz);
+startBtn.addEventListener('click', startQuiz);
 
 startQuiz();
