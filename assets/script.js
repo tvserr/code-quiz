d575d7cd7654c9c08528405dd6adaffda6b130e4
Joel Serrano <theavserrano@gmail.com>
2023-09-20 14:35:09 -0600

    added event listeners for choices

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,18 @@
 var quizContainer = document.querySelector(".quiz-container");
-var questionContainer = document.querySelector(".question-container");
 var choicesContainer = document.querySelector(".choices-container");
 var scoresContainer = document.querySelector(".scores-container");
+var welcomeMessage = document.querySelector(".welcome-message");
 
 var startBtn = document.getElementById("start-quiz");
 var timerDisplay = document.getElementById("timer-display");
-var optionBtn1 = document.getElementById("option1");
-var optionBtn2 = document.getElementById("option2");
-var optionBtn3 = document.getElementById("option3");
-var optionBtn4 = document.getElementById("option4");
-var userScore = document.getElementById("user-score");
 var questionTitle = document.getElementById("question-title");
+var userScore = document.getElementById("user-score");
+var answerResult = document.getElementById("answer-result");
+
+var choiceBtn1 = document.querySelector(".choice-btn1");
+var choiceBtn2 = document.querySelector(".choice-btn2");
+var choiceBtn3 = document.querySelector(".choice-btn3");
+var choiceBtn4 = document.querySelector(".choice-btn4");
 
 var currentQuestion = 0;
 var score = 0;
@@ -29,29 +31,38 @@ var questions = [
    answer: "b. false",
    },
    {
-   title: "In Javascript, this executes a block of code over and over again. What is this called?",
-   choices: ["a. variable loop", "b. function loop", "c. string loop", "d. for loop"],
-   answer: "d. for loop",
+   title: "Arrays in JavaScript can be used to store ______.",
+   choices: ["a. booleans", "b. other arrays", "c. numbers and strings", "d. all of the above"],
+   answer: "d. all of the above",
    },
    {
-   title: "In Javascript, what is this container that stores data?",
+   title: "This executes a block of code over and over again. What is this called?",
+   choices: ["a. variable loop", "b. for loop", "c. string loop", "d. function loop"],
+   answer: "b. for loop",
+   },
+   {
+   title: "What is this container that stores data?",
    choices: ["a. variable", "b. function", "c. string", "d. event"],
    answer: "a. variable",
    },
+   {
+   title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+   choices: ["a. Javascript", "b. terminal/bash", "c. console.log", "d. addEventListener"],
+   answer: "c. console.log",
+   },
 ];
 
-// optionBtn1.addEventListener("click");
-// optionBtn2.addEventListener("click");
-// optionBtn3.addEventListener("click");
-// optionBtn4.addEventListener("click");
+welcomeMessage.style.display = "block";
+quizContainer.style.display = "none";
 
 function startQuiz() {
    currentQuestion = 0;
    score = 0;
    timerCount = 75;
-   startTimer();
+   welcomeMessage.style.display = "none";
+   quizContainer.style.display = "block";
    displayQuestion(currentQuestion);
-
+   startTimer();
 }
 
 function startTimer() {
@@ -59,57 +70,95 @@ function startTimer() {
       timerCount--;
       timerDisplay.textContent = "Time: " + timerCount;
 
-      if (timerCount <= 0) {
+      if (timerCount <= 0 || currentQuestion >= questions.length) {
+         clearInterval(timerInterval);
          endQuiz();
-      }
+      } 
    }, 1000)
 }
 
-function displayQuestion(questionIndex) {
-   if (questionIndex < questions.length) {
-   var currentQuestionObj = questions[questionIndex];
-   questionTitle.textContent = currentQuestionObj.title;
-
-   choicesContainer.innerHTML = "";
-
-   for (var i = 0; i < currentQuestionObj.choices.length; i++) {
-      var choiceBtn = document.createElement("button");
-      choiceBtn.textContent = currentQuestionObj.choices[i];
-      choiceBtn.addEventListener("click", checkAnswer);
-      choicesContainer.appendChild(choiceBtn);
-   }
-   } else {
-   endQuiz();
-   }
+function displayQuestion(currentQuestion) {
+   questionTitle.textContent = questions[currentQuestion].title;
+   choiceBtn1.textContent = questions[currentQuestion].choices[0];
+   choiceBtn2.textContent = questions[currentQuestion].choices[1];
+   choiceBtn3.textContent = questions[currentQuestion].choices[2];
+   choiceBtn4.textContent = questions[currentQuestion].choices[3];
 }
 
-function checkAnswer(event) {
-   var userChoice = event.target.textContent;
-   var currentQuestionObj = questions[currentQuestion];
+choiceBtn1.addEventListener("click", checkAnswer);
+choiceBtn2.addEventListener("click", checkAnswer);
+choiceBtn3.addEventListener("click", checkAnswer);
+choiceBtn4.addEventListener("click", checkAnswer);
 
-   if (userChoice != currentQuestionObj.answer) {
-   timerCount -= 15;
-   userScore.textContent = "Your final score is " + score + ".";
+// if (questionIndex < questions.length) {
+   //    var currentQuestionObj = questions[questionIndex];
+   //    questionTitle.textContent = currentQuestionObj.title;
+
+   //    choicesContainer.innerHTML = "";
+
+   //    for (var i = 0; i < currentQuestionObj.choices.length; i++) {
+   //       var choiceBtn = document.createElement("button");
+   //       choiceBtn.textContent = currentQuestionObj.choices[i];
+   //       choiceBtn.addEventListener("click", checkAnswer);
+   //       choicesContainer.appendChild(choiceBtn);
+   //    }
+   // } else {
+   //    endQuiz();
+
+answerResult.style.display = "none";
+
+function checkAnswer(event) {
+   event.preventDefault();
+
+   answerResult.style.display = "block";
+   setTimeout(function () {
+      answerResult.style.display = "none";
+   }, 1000);
+
+   var userChoice = event.target.textContent;
+
+   if (userChoice == questions[currentQuestion].answer) {
+      score += 5;
+      answerResult.textContent = "Correct!";
+   } else {
+      timerCount -= 15;
+      answerResult.textContent = "Wrong!";
    }
 
    currentQuestion++;
-   displayQuestion(currentQuestion);
+
+   if (currentQuestion < questions.length) {
+      displayQuestion(currentQuestion);
+   } else {
+      endQuiz();
+   }
+}
+
+function getScore() {
+   var storedScore = localStorage.getItem("scoreCount");
+   if (storedScore === null) {
+      score = 0;
+   } else {
+      score = storedScore;
+   }
 }
 
 function showScore() {
-   var correctAnswers = 0;
+   score = 0;
 
-   questions.forEach(function (currentQuestion, currentQuestionIndex) {
-      if (currentQuestion.choices.indexOf(currentQuestion.answer) !== -1) {
-         correctAnswers++;
+   questions.forEach(function (question) {
+      if (currentQuestion.choices.indexOf(question.answer) !== -1) {
+         score += 5;
       }
    })
-
-   scoresContainer.innerHTML = "Your final score is " + correctAnswers + ".";
 }
 
-function endQuiz() {}
+function endQuiz() {
+   quizContainer.style.display = "none";
+   scoresContainer.style.display = "block";
+   scoresContainer.innerHTML = "Your final score is " + score + ".";
 
-startBtn.addEventListener('click', startQuiz);
+   timerDisplay.style.display = "none";
+}
 
-startQuiz();
+startBtn.addEventListener("click", startQuiz);
